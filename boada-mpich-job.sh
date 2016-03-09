4601ebf6ed0dd22bfac096a5479a4bc418abac86
Alex Muntada <alexm@ac.upc.edu>
2016-03-09 12:57:16 +0100

    Import code comment from wiki

--- boada-mpich-job.sh
+++ boada-mpich-job.sh
@@ -8,4 +8,7 @@
 # following option makes sure the job has the same environmnent variables as the submission shell
 #$ -V
 
+# En fer servir "mpirun.mpich2" és imprescindible afegir el paràmetre ''-machinefile'',
+# ja que l'entorn paral·lel "mpich" s'encarrega de preparar l'arxiu "machines" amb
+# la llista de màquines on es pot executar l'aplicació.
 mpirun.mpich2 -np 3 -machinefile $TMPDIR/machines /scratch/boada-1/USERNAME/hello_world_mpi

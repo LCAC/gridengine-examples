25c53f090dcb6b15789be416bacf72c7de4f53e1
Alex Muntada <alexm@ac.upc.edu>
2016-03-09 14:26:34 +0100

    Import arvei generic PE job from wiki

---
+++ arvei-genericpe-job.sh
@@ -0,0 +1,22 @@
+### Directivas para el gestor de colas (opcionales)
+# Cambiar el nombre del trabajo
+#$ -N testgenericpe
+# Especificar un shell
+#$ -S /bin/bash
+
+# Para observar el formato de la variable $PE_HOSTFILE:
+cat $PE_HOSTFILE >  /scratch/nas/1/$USER/genericpe.test
+
+# Miramos el host donde se executa el job de forma inicial:
+EXEC_HOST=`/bin/hostname`
+echo "Master en: $EXEC_HOST" >> /scratch/nas/1/$USER/genericpe.test
+
+# En cada uno de los nodos (incluyendo el job de ejecución) verificamos el hostname:
+for h in `cat $PE_HOSTFILE | cut -f1 -d' ' `; do
+        qrsh -inherit -nostdin -V $h "/bin/hostname | xargs echo 'Ejecutandose en el nodo: ' >> /scratch/nas/1/$USER/genericpe.test_$h" &
+done
+
+# Esperamos a que acaben todos los procesos
+wait
+
+exit 0

cd6152efdc7701224a6efa8d7e0c2d1c299e1bb8
Alex Muntada <alexm@ac.upc.edu>
2016-03-09 18:26:31 +0100

    Import code comment from wiki

--- arvei-mpich-job.sh
+++ arvei-mpich-job.sh
@@ -7,4 +7,8 @@
 # following option makes sure the job has the same environmnent variables as the submission shell
 #$ -V
 
+# Cuando llamamos a mpirun.mpich2, es importante añadir el parámetro
+# "-machinefile $TMPDIR/machines" dado que el entorno paralelo "mpich"
+# se encarga de preparar el archivo "machines" con la lista de máquinas
+# donde se ejecutará la aplicación.
 mpirun.mpich2 -np 3 -machinefile $TMPDIR/machines /scratch/boada-1/USERNAME/hello_world_mpi
